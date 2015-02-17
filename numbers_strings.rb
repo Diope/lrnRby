26b8ecde2695cf41aeff26ed74fd0c3c1ed02b37
Dion <kalani.pettaway@gmail.com>
2015-02-17 07:10:44 -0500

    Player and score

---
+++ numbers_strings.rb
@@ -0,0 +1,14 @@
+# puts 'Mikey loves goonies'
+# puts "Mikey's favorite movie is goonies"
+
+# movie = 'Goonies'
+# rank = 22
+
+# puts "Mikey's favorite movie is #{movie}"
+
+# puts "#{movie} has a rank of #{rank * 2}"
+
+player = "Jerry"
+score = 86
+
+puts "#{player} has a score of #{score}...not bad"
